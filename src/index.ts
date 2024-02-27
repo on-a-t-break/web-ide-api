a83d4b66c7690738561d72cecfbfb5ea2c0401c4
on a t break <kasrahashemi1@gmail.com>
2024-02-27 14:20:19 -0800

    fixes

--- src/index.ts
+++ src/index.ts
@@ -9,11 +9,11 @@ import * as http from 'http';
 import * as WebSocket from 'ws';
 import ChainService from "./services/ChainService";
 import BuildQueueService from "@src/services/BuildQueueService";
+import CleaningService from "@src/services/CleaningService";
 
-
-// if(process.env.SPAM_FAUCET) ChainService.spamFaucet();
+//if(process.env.SPAM_FAUCET) ChainService.spamFaucet();
 BuildQueueService.setup();
-
+//CleaningService.setup();
 
 const httpServer = http.createServer(server);
 const wss = new WebSocket.Server({ server:httpServer});
@@ -22,5 +22,3 @@ wss.on('connection', (ws: WebSocket) => sockets(ws));
 
 httpServer.listen(process.env.WS_PORT || 3001, () => logger.info(`WS LIVE: ${process.env.WS_PORT || 3001}`));
 server.listen(process.env.HTTP_PORT || 3000, () => logger.info(`HTTP LIVE: ${process.env.HTTP_PORT || 3000}`));
-
-// testing
